59883aab3587587312a24e94981caf22dfe0baab
Christopher Stock <email@christopherstock.de>
2020-03-03 15:24:09 +0100

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace DotNetWpfPrimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.Title  = "Test Title 1";
+            this.Width  = 800;
+            this.Height = 600;
+
+
+            InitializeComponent();
+        }
+    }
+}
